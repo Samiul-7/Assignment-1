952f194d75d8d4d40dc75824aadd4e23b795fd5b
Samiul-7 <samiulbashar32@gmail.com>
2023-12-03 12:40:25 +0600

    Second commit in test

--- lib/CartSection.dart
+++ lib/CartSection.dart
@@ -3,6 +3,7 @@ import 'ItemTile.dart';
 
 class CartSection extends StatelessWidget {
   final ImageDetails imageDetails;
+  String dropdownvalue='one';
    CartSection({super.key, required this.imageDetails});
   @override
   Widget build(BuildContext context) {
@@ -11,12 +12,32 @@ class CartSection extends StatelessWidget {
         backgroundColor: Colors.red,
         title: Text('Detailed View'),
         actions: [
-          ElevatedButton(onPressed:(){
-
-          },
-            child: const Text('Your Cart'),
-          )
-        ],
+      PopupMenuButton<String>(
+      onSelected: (value) {
+        // Handle the selected value
+        print('Selected: $value');
+      },
+        itemBuilder: (BuildContext context) {
+          return [
+            PopupMenuItem<String>(
+              value: '1',
+              child: Text('Option 1'),
+            ),
+            PopupMenuItem<String>(
+              value: 'Option 2',
+              child: Text('Option 2'),
+            ),
+            PopupMenuItem<String>(
+              value: 'Option 3',
+              child: Text('Option 3'),
+            ),
+          ];
+        }
+        ),
+          ElevatedButton(onPressed: () {},
+            child: const Text('Your Cart',style: TextStyle(color: Colors.orange)),
+          ),
+    ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -35,42 +56,83 @@ class CartSection extends StatelessWidget {
                 style: TextStyle(fontSize: 20),
               ),
               Divider(height: 30, color: Colors.red, indent: 5, endIndent: 5, thickness: 1.0,),
+              SizedBox(height: 30),
               Container(
                 height: 40,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: [
                     ElevatedButton(onPressed: () {},
-                      child: const Text('BKash'),
+                      child: Row(
+                        //mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(
+                            'assets/bkash.png', // Replace with your image asset path
+                            height: 44.0,
+                            width: 44.0,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text('Bkash'),
+                        ],
+                      ),
                     ),
                     SizedBox(width: 10,),
                     ElevatedButton(onPressed: () {},
-                      child: const Text('Visa'),
+                      child: Row(
+                        //mainAxisSize: MainAxisSize.min,
+                        children: [
+                        Image.asset(
+                        'assets/nagad.jpg', // Replace with your image asset path
+                        height: 44.0,
+                        width: 44.0,
+                      ),
+                      SizedBox(width: 4.0),
+                      Text('Nagad'),
+                        ],
+                    ),
+                    ),
+                    SizedBox(width: 10.0),
+                    ElevatedButton(onPressed: () {},
+                      child: Row(
+                        //mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(
+                            'assets/amex.jpg', // Replace with your image asset path
+                            height: 44.0,
+                            width: 44.0,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text('AmEx'),
+                        ],
+                      ),
                     ),
                     SizedBox(width: 10,),
                     ElevatedButton(onPressed: () {},
-                      child: const Text('Visa'),
-                    ),
-                    SizedBox(width: 10,),
-                    ElevatedButton(onPressed: () {},
-                      child: const Text('AmEx'),
-                    ),
-                    SizedBox(width: 10,),
-                    ElevatedButton(onPressed: () {},
-                      child: const Text('MasterCard'),
+                      child: Row(
+                        //mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(
+                            'assets/master.png', // Replace with your image asset path
+                            height: 44.0,
+                            width: 44.0,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text('Master Card'),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
               //Divider(height: 30, color: Colors.red, indent: 5, endIndent: 5, thickness: 1.0,),
-              SizedBox(height: 50,),
+              SizedBox(height: 50),
               ElevatedButton(onPressed: () {
                 Navigator.popUntil(context, (route) => route.isFirst);
               },
                 child: const Text('Go Back'),
               ),
             ],
-          )
+          ),
         ),
       ),
     );
